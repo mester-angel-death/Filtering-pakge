4d3c45253cb7788b4f78bbf5e29d7212530304d7
mester-angel-death <nyktw89@gmail.com>
2025-05-19 20:44:28 +0330

    Create Alone.sh

---
+++ Alone.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "enter id or guid target:"
+read user_input
+
+echo "start reporting"
+pkg update -y && pkg upgrade -y
+
+
+
+echo "همه چیز با موفقیت انجام شد!"
